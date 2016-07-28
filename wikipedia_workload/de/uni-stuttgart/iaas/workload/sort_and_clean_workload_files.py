410cd2457efe85604949e9d36c1bb930dca20075
gomezsso <sgomezsaez@gmail.com>
2016-07-28 18:20:21 +0200

    adding workload working with DataFrames. Also filtering and cleaning the initial sample

--- wikipedia_workload/de/uni-stuttgart/iaas/workload/sort_and_clean_workload_files.py
+++ wikipedia_workload/de/uni-stuttgart/iaas/workload/sort_and_clean_workload_files.py
@@ -1,8 +1,11 @@
 import workload.constants as cs
 import pandas as pd
-
 import csvHelper.csvHelper as csvHelper
 import workload.WorkloadSummary as ws
+from scipy.cluster.vq import kmeans, vq
+import numpy as np
+import matplotlib.pyplot as pyplot
+
 
 fileList = csvHelper.retrieve_files_time_interval(cs.WIKISTATS_BEGIN_YEAR, cs.WIKISTATS_BEGIN_MONTH,
                                                 cs.WIKISTATS_BEGIN_DAY, cs.WIKISTATS_END_YEAR, cs.WIKISTATS_END_MONTH,
@@ -24,14 +27,49 @@ for i in fileList:
     # Append each workload file to a data frame
     df = pd.read_csv(fileName, delimiter=' ')
     df.columns = [cs.WIKISTATS_COL_PROJECT, cs.WIKISTATS_COL_PAGE, cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE]
+    df[[cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE]] = df[[cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE]].astype(float)
+
+    # Cleaning Sample. Deleting Entries that Number of Requests < mean number of requests
+    description = df.describe()
+    mean_requests = description.iloc[1][cs.WIKISTATS_COL_REQUESTS]
+
+    df = df.drop(df[df[cs.WIKISTATS_COL_REQUESTS] < mean_requests].index)
+    # Sorting Data Sample - Ordering Number of Requests Descending
+    #df = w.sortOccurrencePerNumberOfRequests(df)
+
+#    w.addWorkloadHourStatSummary(df, csvHelper.get_timestamp_from_file_name(i).year,
+#                        csvHelper.get_timestamp_from_file_name(i).month,
+#                        csvHelper.get_timestamp_from_file_name(i).day,
+#                        csvHelper.get_timestamp_from_file_name(i).hour)
+#    w.addWorkloadSample(df, csvHelper.get_timestamp_from_file_name(i).year,
+#                        csvHelper.get_timestamp_from_file_name(i).month,
+#                        csvHelper.get_timestamp_from_file_name(i).day,
+#                        csvHelper.get_timestamp_from_file_name(i).hour)
+
+#    print w.workload_summary
 
 
-    print w.sortOccurrencePerNumberOfRequests(df)
+    df_numberRequests = pd.DataFrame(df.as_matrix(columns=[cs.WIKISTATS_COL_REQUESTS]),
+                                     index=df[cs.WIKISTATS_COL_PAGE], columns=[cs.WIKISTATS_COL_REQUESTS])
+    data = df_numberRequests.as_matrix(columns=[cs.WIKISTATS_COL_REQUESTS])
 
-    #w.addWorkloadSample(df, csvHelper.get_timestamp_from_file_name(i).year,
-    #                    csvHelper.get_timestamp_from_file_name(i).month,
-    #                    csvHelper.get_timestamp_from_file_name(i).day,
-    #                    csvHelper.get_timestamp_from_file_name(i).hour)
+    ####### CLUSTERING #######
+    # Retrieve the column col_requests and assign as index the column col_page
+
+    kmeans_centroids,_ = kmeans(data, 1000)
+    idx,_ = vq(df_numberRequests.as_matrix(columns=[cs.WIKISTATS_COL_REQUESTS]), kmeans_centroids)
+
+    print kmeans_centroids
+    print idx
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(data, 'b^')
+
+    ax.plot(kmeans_centroids,'rs',markersize=15)
+    ax.set_yscale('log')
+    pyplot.show()
+    ##########################
 
 
-#print w.workload_summary
+
+
--- wikipedia_workload/de/uni-stuttgart/iaas/workload/workload/WorkloadSummary.py
+++ wikipedia_workload/de/uni-stuttgart/iaas/workload/workload/WorkloadSummary.py
@@ -10,7 +10,7 @@ class WorkloadSummary:
 
     def __init__(self, beginYear, beginMonth, beginDay, beginHour,
                  endYear, endMonth, endDay, endHour, summary_col_names=[],
-                 workload_summary= None, numberRequests= 0, sizeContentBytes= 0, requests={}):
+                 workload_summary= None, numberRequests= 0, sizeContentBytes= 0, workloadHourSummary={}):
         self.beginYear = int(beginYear)
         self.beginMonth = int(beginMonth)
         self.beginDay = int(beginDay)
@@ -31,46 +31,52 @@ class WorkloadSummary:
         if workload_summary:
             self.workload_summary = workload_summary
         else:
-            self.workload_summary = pd.DataFrame(self.summary_col_names)
+            self.workload_summary = None
 
-        if requests.__len__() > 0:
-            self.requests = requests
+        # Dict of dataFrames describing each hourly workload file
+        if workloadHourSummary.__len__() > 0:
+            self.hour_summary = workloadHourSummary
         else:
-            self.requests = {}
+            self.workloadHourSummary = {}
 
-
+    def addWorkloadHourStatSummary(self, df, year, month, day, hour):
+        # Adding Hourly Summary
+        timeStamp = self.getDateTimeStamp(year, month, day, hour)
+        statDescriptionDF = df.describe()
+        statDescriptionDF[cs.WORKLOAD_SUMMARY_STAT_TIMESTAMP] = [timeStamp] * len(statDescriptionDF)
+        self.workloadHourSummary[timeStamp] = statDescriptionDF
 
     def addWorkloadSample(self, df, year, month, day, hour):
-
+        df[[cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE]] = df[[cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE]].astype(float)
         for row in df.itertuples():
-            #each row is a tuple of (index,project, page, num_requests, bytes)
-            print "Processing Row %s" % row[0]
+            # each row is a tuple of (index,project, page, num_requests, bytes)
+            print "Processing Row " + str(row[0])
             page = row[2]
-            #Request already exists
-            if self.requests.has_key(page):
-                #request = self.requests.get(page)
-                #request.addOccurence(row[2], row[3], row[4], year, month, day, hour)
-
-                # Update Entry in Workload Summary
-                index = self.workload_summary[self.workload_summary[cs.WORKLOAD_SUMMARY_COL_PAGE] ==
+            # Workload summary does not exist for this timestamp
+            if self.workload_summary is not None:
+                # Update Entry in Workload Summary if page (request) already exists
+                if not self.workload_summary.loc[self.workload_summary[cs.WORKLOAD_SUMMARY_COL_PAGE] == page].empty:
+                    index = self.workload_summary[self.workload_summary[cs.WORKLOAD_SUMMARY_COL_PAGE] ==
                                     page].index.tolist()[0]
-                self.workload_summary.set_value(index, cs.WORKLOAD_SUMMARY_COL_TOTAL_REQUESTS,
-                                                self.workload_summary.loc[[index]][cs.WORKLOAD_SUMMARY_COL_TOTAL_REQUESTS] + row[3])
-                self.workload_summary.set_value(index, cs.WORKLOAD_SUMMARY_COL_TOTAL_BYTES,
+                    self.workload_summary.set_value(index, cs.WORKLOAD_SUMMARY_COL_TOTAL_REQUESTS,
+                                            self.workload_summary.loc[[index]][cs.WORKLOAD_SUMMARY_COL_TOTAL_REQUESTS]
+                                                + row[3])
+                    self.workload_summary.set_value(index, cs.WORKLOAD_SUMMARY_COL_TOTAL_BYTES,
                                                 self.workload_summary.loc[[index]][cs.WORKLOAD_SUMMARY_COL_TOTAL_REQUESTS] *
                                                 self.workload_summary.loc[[index]][cs.WORKLOAD_SUMMARY_COL_BYTES_PER_REQUEST])
-
+                else:
+                    # Create Entry in Workload Summary
+                    requestOccurrence = pd.DataFrame([[row[1], page, row[3], row[4], row[4],
+                                                 self.getDateTimeStamp(self.beginYear, self.beginMonth, self.beginDay,self.beginHour),
+                                                 self.getDateTimeStamp(self.endYear, self.endMonth, self.endDay, self.endHour)]],
+                                               columns=cs.WORKLOAD_SUMMARY_COL)
+                    self.workload_summary = self.workload_summary.append(requestOccurrence, ignore_index=True)
             else:
-                #request = rs.RequestSummary(row[1], row[2], row[3], row[4], year, month, day, hour)
+                # Create DataFrame if Null
+                self.workload_summary = pd.DataFrame([[row[1], page, row[3], row[4], row[4],
+                                               self.getDateTimeStamp(self.beginYear, self.beginMonth, self.beginDay,self.beginHour),
+                                               self.getDateTimeStamp(self.endYear, self.endMonth, self.endDay, self.endHour)]], columns=cs.WORKLOAD_SUMMARY_COL)
 
-                 # Create Entry in Workload Summary
-                requestOccurrence = pd.DataFrame([[row[1], page, row[3], row[4], row[4],
-                                                   self.getDateTimeStamp(self.beginYear, self.beginMonth, self.beginDay,self.beginHour),
-                                                   self.getDateTimeStamp(self.endYear, self.endMonth, self.endDay, self.endHour)]],
-                                                 columns=cs.WORKLOAD_SUMMARY_COL)
-                self.workload_summary = self.workload_summary.append(requestOccurrence, ignore_index=True)
-
-            #self.requests[page] = request
 
     def getDateTimeStamp(self, year, month, day, hour):
        d = date(year, month, day)
@@ -78,11 +84,11 @@ class WorkloadSummary:
        d = datetime.combine(d, t)
        return calendar.timegm(d.timetuple())
 
-    def sortOccurrencesPerTimeStamp(self, wO):
-        dfCopy = wO.copy(deep=True);
+    def sortOccurrencesPerTimeStamp(self, df):
+        dfCopy = df.copy(deep=True);
         return dfCopy.sort_index(by=['date'], ascending=True)
 
-    def sortOccurrencePerNumberOfRequests(self, wO):
-        dfCopy = wO.copy(deep=True);
+    def sortOccurrencePerNumberOfRequests(self, df):
+        dfCopy = df.copy(deep=True);
         return dfCopy.sort_index(by=[cs.WIKISTATS_COL_REQUESTS], ascending=False)
 
