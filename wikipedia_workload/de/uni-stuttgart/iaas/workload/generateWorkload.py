819d61ed073e178697f0d1161d4f98e992775abe
gomezsso <sgomezsaez@gmail.com>
2016-08-15 18:01:20 +0200

    workload executor invoking dynamic jmeter test plan executor

--- wikipedia_workload/de/uni-stuttgart/iaas/workload/generateWorkload.py
+++ wikipedia_workload/de/uni-stuttgart/iaas/workload/generateWorkload.py
@@ -3,8 +3,28 @@ import pandas as pd
 import hourly_analysis, usersDailyAnalysis
 import csvHelper.csvHelper as csvHelper
 import numpy as np
+import random as random
 from workload import WorkloadSummary as ws
 from datetime import datetime
+
+
+def generateRequestsDfFromNumber(project, page, request_number, bytes):
+    df = pd.DataFrame(columns=[cs.WIKISTATS_COL_PROJECT, cs.WIKISTATS_COL_PAGE, cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE])
+
+    for i in range(1, request_number):
+        df = df.append(pd.DataFrame(data=[[project, page, 1, bytes, 0]], columns=[cs.WIKISTATS_COL_PROJECT,
+                                                                        cs.WIKISTATS_COL_PAGE,
+                                                                        cs.WIKISTATS_COL_REQUESTS,
+                                                                        cs.WIKISTATS_COL_SIZE,
+                                                                        cs.WIKISTATS_COL_DELAY]), ignore_index=True)
+    return df
+
+def shuffle_data_frame(df):
+    index = list(df.index)
+    random.shuffle(index)
+    df = df.ix[index]
+    df.reset_index()
+    return df
 
 hourly_access_requests_file = cs.DATA_LOCAL_PATH + "1-2016_1-2016_hourly_summary_scaled_factor100Scaling.csv"
 users_daily_access_file = cs.DATA_LOCAL_PATH + 'unique_users_monthly_scaled_factor100.csv'
@@ -52,3 +72,59 @@ df = pd.DataFrame({cs.GENERATED_WORKLOAD_COL_HOURLY_CONCURRENT_USERS : users_per
 hour_total_number_requests[cs.GENERATED_WORKLOAD_COL_HOURLY_CONCURRENT_USERS ] = df[cs.GENERATED_WORKLOAD_COL_HOURLY_CONCURRENT_USERS]
 
 hour_total_number_requests.to_csv(path_or_buf=output_workload_file, sep=' ', index=False)
+
+# Calculating the delay between requests, Uniformely distributed among each hour (3600 sec.) and modifying the original wikipedia page counts workload with the delay
+
+# Generate the list of files to be read
+pageCountFileList = csvHelper.retrieve_files_time_interval(cs.WIKISTATS_BEGIN_YEAR, cs.WIKISTATS_BEGIN_MONTH,
+                                                cs.WIKISTATS_BEGIN_DAY, cs.WIKISTATS_END_YEAR, cs.WIKISTATS_END_MONTH,
+                                                   cs.WIKISTATS_END_DAY, cs.WIKISTATS_HOURS, cs.WIKISTATS_PAGECOUNTS)
+
+# Adapting file list name with scaled suffix
+pageCountFileListSuffix = [(i + cs.WIKISTATS_FILE_SCALED_100_SUFFIX + '.csv') for i in pageCountFileList]
+
+
+# Distributing the requests
+count = 0
+for pageCountFile in pageCountFileListSuffix:
+    print "Processing File: " + pageCountFile
+    pageCountDF = pd.read_csv(cs.DATA_LOCAL_PATH + pageCountFile, delimiter=' ')
+    outDataFrame = pd.DataFrame(columns=[cs.WIKISTATS_COL_PROJECT,
+                                         cs.WIKISTATS_COL_PAGE,
+                                         cs.WIKISTATS_COL_REQUESTS,
+                                         cs.WIKISTATS_COL_SIZE,
+                                         cs.WIKISTATS_COL_DELAY])
+
+    pageCountDF.columns = cs.WIKISTATS_COLUMNS
+
+    for index,row in pageCountDF.iterrows():
+        requests_number = row[cs.WIKISTATS_COL_REQUESTS]
+        if requests_number > 1:
+            requestsDF = generateRequestsDfFromNumber(row[cs.WIKISTATS_COL_PROJECT],
+                                        row[cs.WIKISTATS_COL_PAGE],
+                                         row[cs.WIKISTATS_COL_REQUESTS],
+                                         row[cs.WIKISTATS_COL_SIZE])
+            outDataFrame = outDataFrame.append(requestsDF, ignore_index=True)
+        else:
+            dfAux = pd.DataFrame(data=[[row[cs.WIKISTATS_COL_PROJECT], row[cs.WIKISTATS_COL_PAGE], int(row[cs.WIKISTATS_COL_REQUESTS]), int(row[cs.WIKISTATS_COL_SIZE]), int(0)]],
+                                 columns= [cs.WIKISTATS_COL_PROJECT, cs.WIKISTATS_COL_PAGE, cs.WIKISTATS_COL_REQUESTS, cs.WIKISTATS_COL_SIZE, cs.WIKISTATS_COL_DELAY])
+
+            outDataFrame = outDataFrame.append(dfAux, ignore_index=True)
+
+
+    # Shuffling elements
+
+    outDataFrame = shuffle_data_frame(outDataFrame)
+
+    # Saving to File
+    print "Saving: " + cs.DATA_LOCAL_PATH + cs.WIKISTATS_GENERATED_WORKLOAD_PREFIX + pageCountFileList[count] + cs.WIKISTATS_FILE_SCALED_100_SUFFIX + ".csv"
+    outDataFrame.to_csv(cs.DATA_LOCAL_PATH + cs.WIKISTATS_GENERATED_WORKLOAD_PREFIX + pageCountFileList[count] + cs.WIKISTATS_FILE_SCALED_100_SUFFIX + ".csv", sep=' ', index=False)
+    count += 1
+
+
+    # Calculating and filling the delays
+
+
+
+
+
---
+++ wikipedia_workload/de/uni-stuttgart/iaas/workload/workloadExecutor.py
@@ -0,0 +1,91 @@
+import os
+from workload import constants as cs
+import jprops as jprops
+from csvHelper import csvHelper as csvHelper
+import pandas as pd
+import subprocess
+
+
+def getDateFromFileName(fileName):
+    split1 = fileName.split('-')
+    print split1
+
+# Generate the File List & Read Files
+
+pageCountFileList = csvHelper.retrieve_files_time_interval(cs.WIKISTATS_BEGIN_YEAR, cs.WIKISTATS_BEGIN_MONTH,
+                                                cs.WIKISTATS_BEGIN_DAY, cs.WIKISTATS_END_YEAR, cs.WIKISTATS_END_MONTH,
+                                                   cs.WIKISTATS_END_DAY, cs.WIKISTATS_HOURS, cs.WIKISTATS_PAGECOUNTS)
+
+generatedWorkloadFileList = [(cs.WIKISTATS_GENERATED_WORKLOAD_PREFIX + i + cs.WIKISTATS_FILE_SCALED_100_SUFFIX + '.csv')
+                             for i in pageCountFileList]
+
+
+output_workload_config_file = cs.DATA_LOCAL_PATH + 'workload_hourly_distribution_scaled_factor100.csv'
+df_workload_config = pd.read_csv(output_workload_config_file, delimiter=' ')
+
+# For each generated workload file:
+# 1) Read the Workload Config Parameters for Each Hour and generate a custom config parameters file
+# 2) Execute the jmeter load executer from Sherif
+# 3) Save the result
+
+count_workload_file_list = 0
+for index,row in df_workload_config.iterrows():
+
+    config_properties = {cs.CONFIG_TEST_PLAN_PATH_VAR: cs.CONFIG_TEST_PLAN_PATH_VALUE,
+                         cs.CONFIG_JMETER_PATH_VAR: cs.CONFIG_JMETER_PATH_VALUE}
+
+    scenario_id_value = cs.ARGUMENTS_JMETER_SCENARIO_ID_VALUE + '_' + \
+                        pageCountFileList[count_workload_file_list].split('-')[1] + '-' + \
+                        pageCountFileList[count_workload_file_list].split('-')[2]
+
+    argument_properties = {cs.ARGUMENTS_JMETER_SCENARIO_ID_VAR: scenario_id_value,
+                           cs.ARGUMENTS_JMETER_ROUND_ID_VAR: cs.ARGUMENTS_JMETER_ROUND_ID_VALUE,
+                           cs.ARGUMENTS_JMETER_HTTP_HOST_VAR: cs.ARGUMENTS_JMETER_HTTP_HOST_VALUE,
+                           cs.ARGUMENTS_JMETER_HTTP_PORT_VAR: cs.ARGUMENTS_JMETER_HTTP_PORT_VALUE,
+                           cs.ARGUMENTS_JMETER_HTTP_PATH_VAR: cs.ARGUMENTS_JMETER_HTTP_PATH_VALUE,
+                           }
+
+    print "Processing File: " + generatedWorkloadFileList[count_workload_file_list]
+
+
+    concurrent_users = str(int(row[cs.GENERATED_WORKLOAD_COL_HOURLY_CONCURRENT_USERS]))
+    thread_loop = cs.ARGUMENTS_JMETER_LOOP_PER_THREAD_VALUE
+    thread_rampup = cs.ARGUMENTS_JMETER_THREAD_RAMPUP_VALUE
+    workload_file_path = cs.ARGUMENTS_JMETER_WORKLOAD_CSV_PATH_VALUE + generatedWorkloadFileList[count_workload_file_list]
+    workload_file_num_csv_rows = str(len(pd.read_csv(cs.DATA_LOCAL_PATH + generatedWorkloadFileList[count_workload_file_list], delimiter=' ')))
+    hourly_sum_requests = str(int(row[cs.GENERATED_WORKLOAD_COL_SUM_REQS]))
+
+    argument_properties[cs.ARGUMENTS_JMETER_NUMBER_THREADS_VAR] = concurrent_users
+    argument_properties[cs.ARGUMENTS_JMETER_LOOP_PER_THREAD_VAR] = thread_loop
+    argument_properties[cs.ARGUMENTS_JMETER_THREAD_RAMPUP_VAR] = thread_rampup
+    argument_properties[cs.ARGUMENTS_JMETER_WORKLOAD_CSV_PATH_VAR] = workload_file_path
+    argument_properties[cs.ARGUMENTS_JMETER_WORKLOAD_CSV_NUM_ROWS_VAR] = workload_file_num_csv_rows
+
+    with open('resources/' + cs.LOAD_TEST_CONFIG_FILE, 'w+') as fp:
+      jprops.store_properties(fp, config_properties)
+
+    with open('resources/' + cs.LOAD_TEST_ARGUMENTS_FILE, 'w+') as fp:
+      jprops.store_properties(fp, argument_properties)
+
+    config_properties_file_path = os.getcwd() + "/resources/" + cs.LOAD_TEST_CONFIG_FILE
+    arguments_properties_file_path = os.getcwd() + "/resources/" + cs.LOAD_TEST_ARGUMENTS_FILE
+
+    #subprocess.call(['java', '-jar', cs.JAR_LOAD_TEST_PATH, config_properties_file_path, arguments_properties_file_path])
+
+    count_workload_file_list += 1
+    break
+
+
+
+# Aggregate the summary report files
+# 1) Be able to process the results for each day. The output generated file must follow the same file naming property as the generated load
+# 2) Extract Statistics, e.g. mean response time, mean number of errors
+
+#with open('resources/arguments.properties.template') as fp:
+#  properties = jprops.load_properties(fp)
+
+#x = {'y': '1', 'z': '2'}
+#with open('out.properties', 'w') as fp:
+#  jprops.store_properties(fp, x)
+
+#print properties
